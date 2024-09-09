96ae0e18daf543e6d61811e5bd1817cf9d2734f9
Lalo Alvarez <laloalvarez.e@gmail.com>
2024-09-08 20:20:12 -0600

    First Commit

---
+++ MyFirstServer/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 8 sept 2024
+ *      Author: InDeviceMex
+ */
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char** argv)
+{
+	printf("Hello World, This is the orange PI 5\n");
+	return 0;
+}
